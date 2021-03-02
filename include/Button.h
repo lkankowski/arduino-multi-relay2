c41b75b20e9c5c165cb310a6f43cc5b36db9521d
Leszek Kankowski <lkankowski@gmail.com>
2021-03-02 09:28:29 +0100

    changed expander support

--- include/Button.h
+++ include/Button.h
@@ -65,34 +65,24 @@ typedef struct {
 
 class BounceExp : public Bounce {
   public:
+    BounceExp()
+      : Bounce()
+      , _isExpander(false)
+      , _expanderNo(0)
+    {};
+
     #if defined(EXPANDER_PCF8574)
       static void setExpander(PCF8574 * exp) { _expander = exp; };
     #elif defined(EXPANDER_MCP23017)
       static void setExpander(Adafruit_MCP23017 * exp) { _expander = exp; };
     #endif
 
-    void attach(int pin)
-    {
-      this->pin = pin; // type changed from uint8_t to uint16_t
-      
-      // SET INITIAL STATE
-      begin();
-    };
-
-    void attach(int pin, int mode)
-    {
-      setPinMode(pin, mode);
-      this->attach(pin);
-    };
-
   protected:
     bool readCurrentState() {
 
       int result;
-      if (pin & 0xff00) { // expander
-        int expanderNo = (pin >> 8) - 1;
-        int expanderPin = pin & 0xff;
-        result = _expander[expanderNo].digitalRead(expanderPin);
+      if (_isExpander) {
+        result = _expander[_expanderNo].digitalRead(pin);
       } else {
         result = Bounce::readCurrentState();
       }
@@ -102,16 +92,17 @@ class BounceExp : public Bounce {
     void setPinMode(int _pin, int mode) {
 
       if (_pin & 0xff00) { // expander
-        int expanderNo = (_pin >> 8) - 1;
+        _isExpander = true;
+        _expanderNo = (_pin >> 8) - 1;
         int expanderPin = _pin & 0xff;
         #if defined(EXPANDER_MCP23017)
           // https://github.com/adafruit/Adafruit-MCP23017-Arduino-Library/blob/1.2.0/Adafruit_MCP23017.cpp#L145
           // pinMode method doesn't support INPUT_PULLUP, workaround below:
           if (mode == INPUT_PULLUP) {
-            _expander[expanderNo].pinMode(expanderPin, INPUT);
-            _expander[expanderNo].pullUp(expanderPin, HIGH);
+            _expander[_expanderNo].pinMode(expanderPin, INPUT);
+            _expander[_expanderNo].pullUp(expanderPin, HIGH);
           } else {
-            _expander[expanderNo].pinMode(expanderPin, mode);
+            _expander[_expanderNo].pinMode(expanderPin, mode);
           }
         #else
            _expander[expanderNo].pinMode(expanderPin, mode);
@@ -127,7 +118,8 @@ class BounceExp : public Bounce {
       static Adafruit_MCP23017 * _expander;
     #endif
 
-    uint16_t pin;
+    bool _isExpander;
+    uint8_t _expanderNo;
 };
 
 #endif
